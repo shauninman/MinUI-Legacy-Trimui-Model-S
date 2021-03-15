bbfcfe59ca134fc002a4f0d7c8cc3dd7f5f58d7a
Shaun Inman <shaun@panic.com>
2021-03-14 21:18:43 -0400

    added build of neonloop's pcsx_rearmed
    having trouble building his repo so I haven't submodule-ed it yet

---
+++ paks/PlayStation.pak/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+EMU_EXE=pcsx
+EMU_DIR=$(dirname "$0")
+ROM_DIR=${EMU_DIR/.pak/}
+ROM_DIR=${ROM_DIR/Emus/Roms}
+EMU_NAME=${ROM_DIR/\/mnt\/SDCARD\/Roms\//}
+ROM=${1}
+
+HOME="$ROM_DIR"
+cd "$EMU_DIR"
+"$EMU_DIR/$EMU_EXE" -cdfile "$ROM" &> "/mnt/SDCARD/.logs/$EMU_NAME.txt"
