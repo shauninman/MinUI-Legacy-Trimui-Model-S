ee7ce14eeabbfaa2e13cdd58cbdc250588ed661b
Shaun Inman <shaun@panic.com>
2021-03-22 09:17:06 -0400

    only copy over PS bios if not already present

--- paks/PlayStation.pak/post.sh
+++ paks/PlayStation.pak/post.sh
@@ -5,5 +5,7 @@ DIR=$(dirname "$0")
 cd "$DIR"
 
 # create bios folder and copy default bios from nand
-mkdir -p ./bios
-cp "/usr/trimui/bin/pcsx_rearmed/bios/scph5502.bin" ./bios
+if [ !-f ./bios/scph5502.bin ]; then
+	mkdir -p ./bios
+	cp "/usr/trimui/bin/pcsx_rearmed/bios/scph5502.bin" ./bios
+fi
