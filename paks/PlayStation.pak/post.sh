4a6594b97a66d28271bd0d76e3a17b44277e93d1
Shaun Inman <shaun@panic.com>
2021-03-23 22:57:41 -0400

    fixed PSX post install script

--- paks/PlayStation.pak/post.sh
+++ paks/PlayStation.pak/post.sh
@@ -5,7 +5,7 @@ DIR=$(dirname "$0")
 cd "$DIR"
 
 # create bios folder and copy default bios from nand
-if [ !-f ./bios/scph5502.bin ]; then
+if [ ! -f ./bios/scph5502.bin ]; then
 	mkdir -p ./bios
 	cp "/usr/trimui/bin/pcsx_rearmed/bios/scph5502.bin" ./bios
 fi
