b5a9a5a1cea2701486ef24fb3a11cb08fcd9ab3c
Shaun Inman <shaun@panic.com>
2021-03-25 21:16:38 -0400

    added proper Genesis.pak

--- paks/Genesis.pak/launch.sh
+++ paks/Genesis.pak/launch.sh
@@ -8,8 +8,6 @@ ROM_DIR=${ROM_DIR/Emus/Roms}
 EMU_NAME=${ROM_DIR/\/mnt\/SDCARD\/Roms\//}
 ROM=${1}
 
-encode "$ROM"
-
 HOME="$ROM_DIR"
-cd "$HOME"
-"/usr/trimui/bin/$EMU_EXE" "$ROM"  &> "/mnt/SDCARD/.minui/logs/$EMU_NAME.txt"
+cd "$EMU_DIR"
+"$EMU_DIR/$EMU_EXE" "$ROM" &> "/mnt/SDCARD/.minui/logs/$EMU_NAME.txt"
