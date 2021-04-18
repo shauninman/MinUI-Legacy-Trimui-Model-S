e7c6ef702aa72d5095bfd6452d4277050a821ecb
Shaun Inman <shaun@panic.com>
2021-04-17 23:39:46 -0400

    recover if /System is deleted between boots

--- TrimuiUpdate/installer/launch.sh
+++ TrimuiUpdate/installer/launch.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 # .tmp_update/updater (persistent)
 
-/mnt/SDCARD/System/System.pak/launch.sh
+if [ -f /mnt/SDCARD/System/System.pak/launch.sh ]; then
+	/mnt/SDCARD/System/System.pak/launch.sh
+else
+	notify 50 "unable to find MinUI"
+	sleep 2
+	notify 100 "returning to stock"
+	sleep 2
+	notify 100 quit
+	killall -s KILL updateui
+fi
