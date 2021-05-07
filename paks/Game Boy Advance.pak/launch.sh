eaaa6c3fac7a69c0ea658950de04e96c7d6c968c
Shaun Inman <shaun@panic.com>
2021-05-06 23:10:54 -0400

    an actual fix for gba's menu sleep problem

--- paks/Game Boy Advance.pak/launch.sh
+++ paks/Game Boy Advance.pak/launch.sh
@@ -8,6 +8,8 @@ ROM_DIR=${ROM_DIR/Emus/Roms}
 EMU_NAME=${ROM_DIR/\/mnt\/SDCARD\/Roms\//}
 ROM=${1}
 
+needs-swap
+
 HOME="$ROM_DIR"
 cd "$HOME"
 "$EMU_DIR/$EMU_EXE" "$ROM" &> "/mnt/SDCARD/.minui/logs/$EMU_NAME.txt"
