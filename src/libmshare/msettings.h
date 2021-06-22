79986aa2642f554c8e325b667d268b3f0d7f418e
Shaun Inman <shaun@panic.com>
2021-06-21 22:26:54 -0400

    more comments

--- src/libmshare/msettings.h
+++ src/libmshare/msettings.h
@@ -10,7 +10,7 @@ int GetVolume(void);
 void SetRawBrightness(int value); // 0-120
 void SetRawVolume(int value); // 0-100%
 
-void SetBrightness(int value);
-void SetVolume(int value);
+void SetBrightness(int value); // 0-10
+void SetVolume(int value); // 0-20
 
 #endif  // msettings_h__
